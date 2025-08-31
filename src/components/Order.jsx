0959984b83ec5dea6dd32e1f47d310389e571969
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-09-01 01:24:49 +0530

    ,

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -4,23 +4,11 @@ function OrderHistory({ orders }) {
     <div>
       <h2>Your Order History</h2>
       <ul>
-        {console.log(orders)}
         {orders.map((order) => (
           <li key={order._id} className="mb-6 border-b pb-4">
             <div>
               <strong>Order ID:</strong> {order._id} <br />
-              <strong>Total:</strong> {order.total} <br />
               <strong>Order Status:</strong> {order.status}
-            </div>
-            <div className="mt-2">
-              <strong>Order Items:</strong>
-              <ul className="ml-4 list-disc">
-                {order.items.map((item, idx) => (
-                  <li key={idx}>
-                    {item.product?.name || item.name} &times; {item.quantity}
-                  </li>
-                ))}
-              </ul>
             </div>
           </li>
         ))}
