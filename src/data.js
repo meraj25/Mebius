cfe776b358b2f2fae7abfc7eebc17f4613ac2532
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-09-06 11:43:09 +0530

    ,

--- src/data.js
+++ src/data.js
@@ -1,271 +1,225 @@
 const products = [
-  {
-    _id: "67def6e42b2c146578729f60",
-    categoryId: "67dee9c012d36efdd1027b43",
-    colorId: "67deec8e2b2c146578729f2f",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/f93fec70-da18-44f9-a18d-27a5edd24037/M+NK+DF+FORM+7IN+UL+SHORT.png",
-    name: "Nike Dri-FIT Training Shorts",
-    price: 40,
-    stripePriceId: "price_1R5W5XJjbWEvglIU42i6PuXC",
-    description:
-      "Lightweight and breathable training shorts designed for high-performance workouts.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67def6f32b2c146578729f64",
-    categoryId: "67dee9c012d36efdd1027b43",
-    colorId: "67deecb02b2c146578729f31",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4d2bc9f3-6a55-4cd0-81f1-f1bd9bdb9723/M+NK+CLUB+FLOW+SHORT.png",
-    name: "Nike Sportswear Woven Shorts",
-    price: 50,
-    stripePriceId: "price_1R5W5mJjbWEvglIUb5WgcWAl",
-    description:
-      "Casual woven shorts with a comfortable fit and modern aesthetic.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67def70b2b2c146578729f68",
-    categoryId: "67dee9c012d36efdd1027b43",
-    colorId: "67deec642b2c146578729f2b",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0588a1a7-f96c-4983-b10e-50e0dc505e45/M+NK+DF+WVN+ICON+6IN+SHORT+STF.png",
-    name: "Nike Basketball Dri-FIT Shorts",
-    price: 45,
-    stripePriceId: "price_1R5W6AJjbWEvglIU8A9pGc3d",
-    description:
-      "Basketball shorts with moisture-wicking fabric and a relaxed fit for game-time comfort.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defdfed2a89836105f675c",
-    categoryId: "67dee9cd12d36efdd1027b45",
-    colorId: "67deecb02b2c146578729f31",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/674515e1-3d58-4635-a701-7d7e6c37b4c0/M+NK+DF+PRIMARY+STMT+SS.png",
-    name: "Nike Dri-FIT Training T-Shirt",
-    price: 35,
-    stripePriceId: "price_1R5WYrJjbWEvglIUpETkqOLk",
-    description:
-      "A sweat-wicking training t-shirt designed for comfort and mobility during workouts.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe10d2a89836105f6760",
-    categoryId: "67dee9cd12d36efdd1027b45",
-    colorId: "67deec8e2b2c146578729f2f",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/553a3029-81d3-48c5-a176-6a98c9392e41/M+NSW+TEE+M90+OC+HBR+PK4.png",
-    name: "Nike Air Max Graphic T-Shirt",
-    price: 30,
-    stripePriceId: "price_1R5WZ9JjbWEvglIUMd2m0UHc",
-    description:
-      "A casual t-shirt featuring bold Air Max graphic designs for a sporty look.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe1fd2a89836105f6764",
-    categoryId: "67dee9cd12d36efdd1027b45",
-    colorId: "67deecd52b2c146578729f33",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/0e41a9d9-029d-4ec5-af19-182ee2b5721e/M+NSW+TEE+ICON+SWOOSH.png",
-    name: "Nike Sportswear Swoosh T-Shirt",
-    price: 28,
-    stripePriceId: "price_1R5WZOJjbWEvglIUqdkyCI8m",
-    description:
-      "A classic Nike t-shirt with a large Swoosh logo for everyday wear.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe31d2a89836105f6768",
-    categoryId: "67deea2812d36efdd1027b47",
-    colorId: "67deec8e2b2c146578729f2f",
-    image:
-      "https://static.nike.com/a/images/t_prod/w_1920,c_limit,f_auto,q_auto/ebaf91e3-8e79-4dac-8947-169111f38fff/image.jpg",
-    name: "Nike Sportswear Club Joggers",
-    price: 60,
-    stripePriceId: "price_1R5WZhJjbWEvglIU9iMkrCvE",
-    description:
-      "Soft joggers with a relaxed fit, perfect for casual or active days.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe44d2a89836105f676c",
-    categoryId: "67deea2812d36efdd1027b47",
-    colorId: "67deec7f2b2c146578729f2d",
-    image:
-      "https://static.nike.com/a/images/t_prod/w_1920,c_limit,f_auto,q_auto/c25018a6-af5e-4f02-afff-aef9c5f1ae2c/pdp.jpg",
-    name: "Nike ACG Trail Pants",
-    price: 120,
-    stripePriceId: "price_1R5WZzJjbWEvglIUg0EBCYLy",
-    description:
-      "Durable pants designed for hiking and outdoor activities with adjustable cuffs for a custom fit.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe57d2a89836105f6770",
-    categoryId: "67deea2812d36efdd1027b47",
-    colorId: "67deec8e2b2c146578729f2f",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8e169e49-e6dc-4f84-9468-4d8b0864b997/M+NK+DF+PHENOM+ELITE+KNIT+PANT.png",
-    name: "Nike Training Essentials Pants",
-    price: 70,
-    stripePriceId: "price_1R5WaIJjbWEvglIUJtkpTQNH",
-    description:
-      "Everyday training pants with moisture-wicking fabric and a comfortable fit.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe6ad2a89836105f6774",
-    categoryId: "67deea3012d36efdd1027b49",
-    colorId: "67deecd52b2c146578729f33",
-    image:
-      "https://i8.amplience.net/i/jpl/sz_506453_a?qlt=92&w=600&h=464&v=1&fmt=auto",
-    name: "Nike Everyday Plus Cushioned Socks",
-    price: 18,
-    stripePriceId: "price_1R5WabJjbWEvglIUEEMDw6M6",
-    description:
-      "Comfortable cushioned socks designed for everyday wear and athletic performance.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe79d2a89836105f6778",
-    categoryId: "67deea3012d36efdd1027b49",
-    colorId: "67deec642b2c146578729f2b",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/78e7ff88-6ef9-49e1-bdaf-336d4f40e08a/U+NK+UNICORN+CSH+AKL+1PR+-+160.png",
-    name: "Nike Dri-FIT Running Socks",
-    price: 20,
-    stripePriceId: "price_1R5WaqJjbWEvglIUsLDTPzUm",
-    description:
-      "Breathable socks designed to keep feet dry during intense running sessions.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67defe8bd2a89836105f677c",
-    categoryId: "67deea3012d36efdd1027b49",
-    colorId: "67deec8e2b2c146578729f2f",
-    image:
-      "https://cdn.media.amplience.net/i/pgatss/2000000002501-57-01_pc?$large$&fmt=auto",
-    name: "Nike Performance Crew Socks",
-    price: 18,
-    stripePriceId: "price_1R5Wb8JjbWEvglIUvBRSgQQn",
-    description:
-      "High-performance crew socks made from moisture-wicking fabric for enhanced comfort.",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df89a1ec2e6e61d4c61477",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/2507976b1988e6d9a08599fcba5247bd.png",
-    name: "Casual Shoe",
-    price: 225,
-    stripePriceId: "price_1R5frQJjbWEvglIUYkqeE47v",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df89d5ec2e6e61d4c6147c",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/5c482fd10ff96fad137ae5ca5aa7a561.png",
-    name: "Skateboard Shoe",
-    price: 125,
-    stripePriceId: "price_1R5fsGJjbWEvglIU8z1ovxyc",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df89f5ec2e6e61d4c61481",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/02536ab1a554d315fc11f4ef6f9c1302.png",
-    name: "Air Skateboard Shoe",
-    price: 125,
-    stripePriceId: "price_1R5fslJjbWEvglIUv3hn9Qdo",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df8ab6ec2e6e61d4c6148f",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/10fd34a19ed897401decf4c2dd4806d5.png",
-    name: "High Sole Skateboard Shoe",
-    price: 235,
-    stripePriceId: "price_1R5fvtJjbWEvglIUU6iFzg4h",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df8accec2e6e61d4c61491",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/df950869c406241f2f48f416d0807241.png",
-    name: "Basket Shoe",
-    price: 125,
-    stripePriceId: "price_1R5fwFJjbWEvglIUVm9hT3cQ",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
-  {
-    _id: "67df8ba4ec2e6e61d4c61499",
-    categoryId: "67dee99f12d36efdd1027b40",
-    image: "/assets/images/476b2a224ae85cd40fd6b1c7d34bc9ae.png",
-    name: "Sportwear Shoe",
-    price: 125,
-    stripePriceId: "price_1R5fzjJjbWEvglIUsdz5PhEM",
-    description: "Lorem ipsum",
-    featured: true,
-    __v: 0,
-  },
+   {
+        "reviews": [],
+        "_id": "68ab1920f402d03d92bd215a",
+        "categoryId": "68ab17aaf402d03d92bd2155",
+        "name": "Adidas Mens Shoes",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 100,
+        "image": "https://cdn.mos.cms.futurecdn.net/v2/t:0,l:937,cw:2409,ch:2409,q:80,w:2409/CQHiUwBDFpz37fibh6rRUQ.jpg",
+        "stock": 10
+    },
+    {
+        "reviews": [],
+        "_id": "68ab1a2bf402d03d92bd215b",
+        "categoryId": "68ab17c1f402d03d92bd2156",
+        "name": "Mens Tshirt",
+        "color": "68ad85bd1ee7f95281e4d19c",
+        "price": 10,
+        "image": "https://tshirtrepublic.lk/wp-content/uploads/2020/01/premium-crew-neck-tshirt-men-raspberry-red-tshirt-republic-sri-lanka.jpg",
+        "stock": 20
+    },
+    {
+        "reviews": [],
+        "_id": "68ab3775084fd16ae387939b",
+        "categoryId": "68ab1854f402d03d92bd2157",
+        "name": "Nike Shorts",
+        "color": "68ad85871ee7f95281e4d196",
+        "price": 10,
+        "image": "https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/e73494b7-a635-49e6-b9e9-e5e216d83e58/AS+M+NK+DF+CHALLENGER+9UL+SH.png",
+        "stock": 20
+    },
+    {
+        "reviews": [],
+        "_id": "68ab38c4084fd16ae387939c",
+        "categoryId": "68ab1875f402d03d92bd2158",
+        "name": "Adidas Pants",
+        "color": "68ad835e1ee7f95281e4d190",
+        "price": 21,
+        "image": "https://cdn-images.farfetch-contents.com/21/44/30/97/21443097_51405092_600.jpg",
+        "stock": 8
+    },
+    {
+        "reviews": [],
+        "_id": "68ab3942084fd16ae387939d",
+        "categoryId": "68ab18caf402d03d92bd2159",
+        "name": "Nike Socks for Men",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 4,
+        "image": "https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/b3a467fb-4c84-44b0-9be9-589391fa2065/U+NK+EVERYDAY+LTWT+CREW+3PR.png",
+        "stock": 10
+    },
+    {
+        "_id": "68b45e09520fa4d672212399",
+        "categoryId": "68ab17aaf402d03d92bd2155",
+        "name": "Nike Red shoes",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 23,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/16e37ab0-54f3-44fa-9f0a-911f3561a3c0",
+        "stock": 3,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcb82e8d102656c1b3862",
+        "categoryId": "68ab17aaf402d03d92bd2155",
+        "name": "Nike air Zoom",
+        "color": "68ad85871ee7f95281e4d196",
+        "price": 50,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/5bec0e70-8a7e-49ab-b244-b231ba7de519",
+        "stock": 20,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcbf1e8d102656c1b3864",
+        "categoryId": "68ab17aaf402d03d92bd2155",
+        "name": "Nike Sport Shoe",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 110,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/41ee4ff6-c954-4e4a-b91c-6ec8bfd0b9d5",
+        "stock": 5,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcc67e8d102656c1b3866",
+        "categoryId": "68ab17aaf402d03d92bd2155",
+        "name": "Puma smart shoes",
+        "color": "68ad85521ee7f95281e4d194",
+        "price": 45,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/b9f22552-b50c-4ff3-9fca-73c9a2a52c44",
+        "stock": 6,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcd38e8d102656c1b3868",
+        "categoryId": "68ab17c1f402d03d92bd2156",
+        "name": "Adidas cotton T",
+        "color": "68ad835e1ee7f95281e4d190",
+        "price": 25,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/8a51d00c-6166-4ad9-96c0-24e6e09efd86",
+        "stock": 5,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcd68e8d102656c1b386a",
+        "categoryId": "68ab17c1f402d03d92bd2156",
+        "name": "Adidas Default Red",
+        "color": "68ad85521ee7f95281e4d194",
+        "price": 28,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/ca91f172-19dd-4ca1-9fd5-585c57388417",
+        "stock": 3,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcdc7e8d102656c1b386c",
+        "categoryId": "68ab17c1f402d03d92bd2156",
+        "name": "Adidas camo ",
+        "color": "68ad861c1ee7f95281e4d1a5",
+        "price": 30,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/7b9c77aa-4ec1-4c56-8c8e-b97f684e4bab",
+        "stock": 11,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcdffe8d102656c1b386e",
+        "categoryId": "68ab17c1f402d03d92bd2156",
+        "name": "Nike Legends men",
+        "color": "68ad85f11ee7f95281e4d1a2",
+        "price": 20,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/01b71d8c-2d41-487d-81c2-dee9fe2a59ac",
+        "stock": 20,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbce7fe8d102656c1b3870",
+        "categoryId": "68ab1854f402d03d92bd2157",
+        "name": "Nike Club short",
+        "color": "68ad861c1ee7f95281e4d1a5",
+        "price": 18,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/7acd5415-def5-4d1b-bf00-1652fa45ab26",
+        "stock": 15,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbceb5e8d102656c1b3872",
+        "categoryId": "68ab1854f402d03d92bd2157",
+        "name": "Nike Womens sport short",
+        "color": "68ad85521ee7f95281e4d194",
+        "price": 12,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/e8b87f36-d177-4c8f-ba6d-a6302668e069",
+        "stock": 6,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcf06e8d102656c1b3874",
+        "categoryId": "68ab18caf402d03d92bd2159",
+        "name": "Adidas Classic Socks",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 4,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/59903651-f5e3-4a01-a593-18e25eac46fe",
+        "stock": 30,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcf3ee8d102656c1b3876",
+        "categoryId": "68ab18caf402d03d92bd2159",
+        "name": "Adidas Classic Half Socks",
+        "color": "68ad853b1ee7f95281e4d192",
+        "price": 6,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/8ece9f0a-7d93-4209-b9ee-5872cf724745",
+        "stock": 35,
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "68bbcf69e8d102656c1b3878",
+        "categoryId": "68ab18caf402d03d92bd2159",
+        "name": "Adidas arctic socks",
+        "color": "68ad85871ee7f95281e4d196",
+        "price": 8,
+        "image": "https://pub-e27ce3aeb84f4962aba3b1ef90fcf2ba.r2.dev/efbc244a-9fcf-4683-8b62-1c8b0a5df35f",
+        "stock": 35,
+        "reviews": [],
+        "__v": 0
+    }
 ];
 
 const categories = [
-  {
-    _id: "67dee99f12d36efdd1027b40",
-    name: "Shoes",
-    slug: "shoes",
-    __v: 0,
-  },
-  {
-    _id: "67dee9c012d36efdd1027b43",
-    name: "Shorts",
-    slug: "shorts",
-    __v: 0,
-  },
-  {
-    _id: "67dee9cd12d36efdd1027b45",
-    name: "T-shirts",
-    slug: "tshirts",
-    __v: 0,
-  },
-  {
-    _id: "67deea2812d36efdd1027b47",
-    name: "Pants",
-    slug: "pants",
-    __v: 0,
-  },
-  {
-    _id: "67deea3012d36efdd1027b49",
-    name: "Socks",
-    slug: "socks",
-    __v: 0,
-  },
-];
+    {
+        "_id": "68ab17aaf402d03d92bd2155",
+        "name": "Shoes",
+        "slug": "shoes"
+    },
+    {
+        "_id": "68ab17c1f402d03d92bd2156",
+        "name": "T-Shirts",
+        "slug": "tshirts"
+    },
+    {
+        "_id": "68ab1854f402d03d92bd2157",
+        "name": "Shorts",
+        "slug": "shorts"
+    },
+    {
+        "_id": "68ab1875f402d03d92bd2158",
+        "name": "Pants",
+        "slug": "pants"
+    },
+    {
+        "_id": "68ab18caf402d03d92bd2159",
+        "name": "Socks",
+        "slug": "socks"
+    }
+]
 
 export { products, categories };
