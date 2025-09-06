f79e78825bb8cfef8b5abd919a36039b87c2cacf
meraj25 <tambdevapriya@students.nsbm.ac.lk>
2025-09-06 11:54:40 +0530

    ,

--- src/components/HeroGrid.jsx
+++ src/components/HeroGrid.jsx
@@ -4,7 +4,7 @@ function HeroGrid() {
     <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4 lg:px-16 min-h-[60vh] md:min-h-[80vh] gap-4 mt-4">
       <div className="relative col-span-1 lg:col-span-2 rounded-2xl">
         <img
-          src={"/assets/images/729091cd0452fb9d0b89106ceec16368.png"}
+          src={"/assets/images/public/assets/images/FALL_16_CAMPAIGN___THE_RANCH_1759_B.0.jpeg"}
           className="rounded-2xl w-full h-full object-cover"
           alt="hero"
         />
@@ -22,7 +22,7 @@ function HeroGrid() {
       <div className="col-span-1 grid grid-rows-1 md:grid-rows-2 gap-4">
         <div className="rounded-2xl relative h-40 md:h-auto">
           <img
-            src="/assets/images/29a85f64d93c41afa6b64d31b3a88038.png"
+            src="/assets/images/public/assets/images/100850333-aa1be679-46f7-4c9b-8432-09467a9fa693.jpg"
             alt="Featured product"
             className="rounded-2xl w-full h-full object-cover"
           />
@@ -34,7 +34,7 @@ function HeroGrid() {
         </div>
         <div className="rounded-2xl relative h-40 md:h-auto">
           <img
-            src="/assets/images/0233936f837e7b69d6a545511b1ba132.png"
+            src="/assets/images/public/assets/images/istockphoto-494349086-612x612.jpg"
             alt="Featured product"
             className="rounded-2xl w-full h-full object-cover"
           />
